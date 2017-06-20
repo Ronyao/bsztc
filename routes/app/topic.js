dce310f707fff9e9d9c9e7fc50e4d2cb8ef28806
谭荣耀 <ronyao@aliyun.com>
2017-06-20 17:06:41 +0800

    搜索进入信息bug

--- routes/app/topic.js
+++ routes/app/topic.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var AV = require('leanengine');
-var sleep = require('sleep');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
